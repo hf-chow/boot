d23d111e6ce185be1c412b922517e84b1ee5a8f3
Hon Chow <honfungchow@gmail.com>
2024-02-07 00:04:47 +0800

    spread update

--- alg/spread.py
+++ alg/spread.py
@@ -1,2 +1,8 @@
 def get_estimated_spread(audiences_followers):
-    pass
+    s = sum(audiences_followers)
+    n = len(audiences_followers)
+    return (s/n)*(n**1.2)
+
+test_list = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+assert round(get_estimated_spread(test_list)) == 872
+
