cd5cfbfa68dfbf8f5ff5023898636f40fe64c940
Hon Chow <honfungchow@gmail.com>
2024-02-19 22:59:50 +0800

    started ds

---
+++ ds/stack.py
@@ -0,0 +1,24 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        if len(self.stack) == 0:
+            return None
+        else:
+            return self.stack.pop()
+
+    def peek(self):
+        if len(self.stack) == 0:
+            return None
+        else:
+            return self.stack[-1]
+    
+    def size(self):
+        return len(self.stack)
+
+
+
