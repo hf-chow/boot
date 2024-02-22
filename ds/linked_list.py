54502615244be920f5c3daed89e42e96c1ec69ec
Hon Chow <honfungchow@gmail.com>
2024-02-23 00:00:01 +0800

    added linkedlist

---
+++ ds/linked_list.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+    def set_next(self, node):
+        self.next = node
+
+    def __repr__(self):
+        return self.val
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __iter__(self):
+        node = self.head
+        while node != None:
+            yield node
+            node = node.next
+
+    def __repr__(self):
+        nodes = []
+        current = self.head
+        while current and hasattr(current, "val"):
+            nodes.append(current.val)
+            current = current.next
+        return " -> ".join(nodes)
+
+    def add_to_tail(self, node):
+        self.head = node
