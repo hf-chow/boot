69c3513c5d154bc4192fc8076c3590923ac9f1df
Hon Chow <honfungchow@gmail.com>
2024-04-21 02:07:21 +0800

    added text_to_textnodes

--- static_site/src/inline.py
+++ static_site/src/inline.py
@@ -113,3 +113,11 @@ def split_nodes_link(old_nodes):
 
     return new_nodes
 
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
+    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
